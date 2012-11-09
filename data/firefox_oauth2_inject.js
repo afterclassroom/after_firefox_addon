c62034746d8f1d334e6f014e761092940257c2b0
Dat Ngo <ngothiendat@gmail.com>
2012-11-09 17:03:10 +0700

    update new xpi

--- data/firefox_oauth2_inject.js
+++ data/firefox_oauth2_inject.js
@@ -1,6 +1,6 @@
 var url = window.location.href;
 
-if (url.indexOf('http://dev.afterclassroom.com/robots.txt') == 0){
+if (url.indexOf('http://pesome.com/robots.txt') == 0){
 
     var params = url.substring(url.indexOf('?'));
     self.postMessage(params);

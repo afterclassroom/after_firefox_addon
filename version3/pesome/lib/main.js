6793523a512b4d9feda2c34891ba6d3659b2fcfd
Dat Ngo <ngothiendat@gmail.com>
2012-11-22 14:53:56 +0700

    load btn toolbar

---
+++ version3/pesome/lib/main.js
@@ -0,0 +1,48 @@
+var pp = function(o) {
+    return JSON.stringify(o,null,'  ')
+};
+
+exports.main = function(options) {
+    const data = require("self").data;
+    var activeUrl = "";
+    var authenticationToken = undefined;
+    var loadingImg = "";
+
+
+
+    loadingImg = data.url("loading.gif");
+
+    var after_panel = require("panel").Panel({
+        width: 565,
+        height: 580,
+        contentURL: data.url("pages/index.html"),
+        //        contentScriptFile: [data.url("jquery-1.7.1.min.js")
+        //        ,data.url("firefox_oauth2.js")
+        //        ,data.url("select2.js")
+        //        ,data.url("bootstrap-modal.js")
+        //        ],
+        onShow: function() {
+        },
+        onHide: function() {
+        }
+    });
+
+    var btn = require("toolbarbutton").ToolbarButton({
+        id: 'my-toolbar-button',
+        label: 'PeTick',
+        tooltiptext: 'PeTick',
+        image: data.url('img/icon_16.png'),
+        panel: after_panel,
+        onCommand: function() {
+            //after_panel.port.emit('beginOauth', null);
+            activeUrl = require("tabs").activeTab.url;
+        }
+    });
+    if (options.loadReason === "startup") {
+        btn.moveTo({
+            toolbarID: "nav-bar",
+            forceMove: false //only move from palette
+        });
+    }
+
+};

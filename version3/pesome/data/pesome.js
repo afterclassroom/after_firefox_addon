60867f2fb4700bba874ae40a03bd95545d65925b
Dat Ngo <ngothiendat@gmail.com>
2012-11-23 08:46:51 +0700

    signin success

--- version3/pesome/data/pesome.js
+++ version3/pesome/data/pesome.js
@@ -6,17 +6,17 @@ self.port.on("InitContent", function(response) {
     params_arr.push(str_action);
 
     $('#main_panel').find('#btn_sign_in').click(function(){
-        console.log('us name '+$('#main_panel').find('#user_email').val());
-        console.log('us psw '+$('#main_panel').find('#user_password').val());
-        console.log('');
         params_arr.push($('#main_panel').find('#user_email').val());
         params_arr.push($('#main_panel').find('#user_password').val());
-
+        $('#main_panel').css('display','none');
 
         self.port.emit('SignIn', params_arr);
     });
     $('#main_panel').css('display','');
 });
+
 self.port.on("AfterSignin", function(response) {
     console.log('after signin');
+    $('#main_panel').html(response);
+    $('#main_panel').css('display','');
 });

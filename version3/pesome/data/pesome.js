fd41c8c32cb6bcbe417c282644ceb095311c36b3
Dat Ngo <ngothiendat@gmail.com>
2012-12-01 08:47:46 +0700

    update

--- version3/pesome/data/pesome.js
+++ version3/pesome/data/pesome.js
@@ -88,67 +88,68 @@ function PostLinkHandler(response){
             $('#main_panel').find('#images_carousel').show();
         }
     });
-//END HANDLER for checkbox click and reclick
+    //END HANDLER for checkbox click and reclick
 
-var str_action = $('#main_panel').find('#form_tick').attr('action');
-$('#main_panel').find('#send_bt').click(function(){
-    //BEGIN validation for valid form before submit
-    if ( $('input[name="classroom_ids[]"]:checked').length > 0 ){
-        //            var cls_list = [];
-        var params_arr = [];
-        //            $('input[name="classroom_ids[]"]:checked').each(function(i){
-        //                cls_list.push($(this).val());
-        //            });
-        //            var tags = $('#tags').val();
-        //            params_arr.push(cls_list);
-        //            params_arr.push(tags);
-        //
-        //            if ($('#main_panel').find('#title').val() != '' ){
-        //                params_arr.push($('#title').val());
-        //            } else {
-        //                params_arr.push("No title");
-        //            }
-        params_arr.push(str_action);
-        var values = $('#main_panel').find('#form_tick').serialize();
-        params_arr.push(values);
-        //BEGIN handle checkbox No thumbnail
-        if ($('#main_panel').find('#fldcheckbox').is(':checked')) {
-            $('#link_image').remove();
+    var str_action = $('#main_panel').find('#form_tick').attr('action');
+    $('#main_panel').find('#send_bt').click(function(){
+        //BEGIN validation for valid form before submit
+        if ( $('input[name="classroom_ids[]"]:checked').length > 0 ){
+            //            var cls_list = [];
+            var params_arr = [];
+            //            $('input[name="classroom_ids[]"]:checked').each(function(i){
+            //                cls_list.push($(this).val());
+            //            });
+            //            var tags = $('#tags').val();
+            //            params_arr.push(cls_list);
+            //            params_arr.push(tags);
+            //
+            //            if ($('#main_panel').find('#title').val() != '' ){
+            //                params_arr.push($('#title').val());
+            //            } else {
+            //                params_arr.push("No title");
+            //            }
+            //BEGIN handle checkbox No thumbnail
+            if ($('#main_panel').find('#fldcheckbox').is(':checked')) {
+                $('#link_image').remove();
+            }
+            //LOADING WAIT...
+            params_arr.push(str_action);
+            var values = $('#main_panel').find('#form_tick').serialize();
+            params_arr.push(values);
+
+            $('#main_panel').html(loading_Image);
+            self.port.emit('SubmitTick', params_arr);
+        }else {
+            $('#alertModal').modal('show');
         }
-        //LOADING WAIT...
-        $('#main_panel').html(loading_Image);
-        self.port.emit('SubmitTick', params_arr);
-    }else {
-        $('#alertModal').modal('show');
-    }
-//END validation for valid form before submit
-});
+    //END validation for valid form before submit
+    });
     
-//BEGIN link handler
-var str_link =  $('#main_panel').find('.signout').attr('href');
-$('#main_panel').find('.signout').attr('href','javascript:;');
-var mypost_link = $('#main_panel').find('.myTick').attr('href');
-$('#main_panel').find('.myTick').attr('href','javascript:;');
-var home_link = $('#main_panel').find('#linkto_classroom').attr('href');
-$('#main_panel').find('#linkto_classroom').attr('href','javascript:;');
-var original_link = $('#main_panel').find('#link').attr('href');
-$('#main_panel').find('#link').attr('href','javascript:;');
+    //BEGIN link handler
+    var str_link =  $('#main_panel').find('.signout').attr('href');
+    $('#main_panel').find('.signout').attr('href','javascript:;');
+    var mypost_link = $('#main_panel').find('.myTick').attr('href');
+    $('#main_panel').find('.myTick').attr('href','javascript:;');
+    var home_link = $('#main_panel').find('#linkto_classroom').attr('href');
+    $('#main_panel').find('#linkto_classroom').attr('href','javascript:;');
+    var original_link = $('#main_panel').find('#link').attr('href');
+    $('#main_panel').find('#link').attr('href','javascript:;');
 
-$('#main_panel').find('.signout').click(function(){
-    self.port.emit('SignOut', str_link);
-});
-$('#main_panel').find('.myTick').click(function(){
-    self.port.emit('GotoLink', mypost_link);
-});
-$('#main_panel').find('#linkto_classroom').click(function(){
-    self.port.emit('GotoLink', home_link);
-});
-$('#main_panel').find('#link').click(function(){
-    self.port.emit('GotoLink', original_link);
-});
-//END link handler
+    $('#main_panel').find('.signout').click(function(){
+        self.port.emit('SignOut', str_link);
+    });
+    $('#main_panel').find('.myTick').click(function(){
+        self.port.emit('GotoLink', mypost_link);
+    });
+    $('#main_panel').find('#linkto_classroom').click(function(){
+        self.port.emit('GotoLink', home_link);
+    });
+    $('#main_panel').find('#link').click(function(){
+        self.port.emit('GotoLink', original_link);
+    });
+    //END link handler
 
     
-$('#main_panel').css('display','');
+    $('#main_panel').css('display','');
 
 }
